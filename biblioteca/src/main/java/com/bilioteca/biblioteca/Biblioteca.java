40620c43064510733c5796f82dafca1a81c518da
JhonFanor <jforjuela@unillanos>
2023-09-25 17:37:02 -0500

    Avance

---
+++ biblioteca/src/main/java/com/bilioteca/biblioteca/Biblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bilioteca.biblioteca;
+
+/**
+ *
+ * @author jhon
+ */
+public class Biblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
