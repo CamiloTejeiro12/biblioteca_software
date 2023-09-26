b8300770dabdd9cf132ad7963aabbe558e450a9e
JhonFanor <jforjuela@unillanos>
2023-09-25 19:40:25 -0500

    Avance

--- biblioteca/src/main/java/com/bilioteca/biblioteca/Biblioteca.java
+++ biblioteca/src/main/java/com/bilioteca/biblioteca/Biblioteca.java
@@ -1,16 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.bilioteca.biblioteca;
 
-/**
- *
- * @author jhon
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
 public class Biblioteca {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Connection conexion = ConexionBDUtil.obtenerConexion();
+        try {
+            String consultaSQL = "SELECT usuario.id, usuario.nombre_usuario, usuario.\"contraseña\", persona.nombre, persona.direccion " +
+                                 "FROM usuario " +
+                                 "JOIN persona ON usuario.persona = persona.cedula";
+            PreparedStatement statement = conexion.prepareStatement(consultaSQL);
+
+            // Ejecutar la consulta y obtener el conjunto de resultados
+            ResultSet resultSet = statement.executeQuery();
+
+            // Procesar y mostrar los resultados
+            while (resultSet.next()) {
+                String idUsuario = resultSet.getString("id");
+                String nombreUsuario = resultSet.getString("nombre_usuario");
+                String contraseña = resultSet.getString("contraseña");
+                String nombrePersona = resultSet.getString("nombre");
+                String direccionPersona = resultSet.getString("direccion");
+
+                System.out.println("ID de Usuario: " + idUsuario);
+                System.out.println("Nombre de Usuario: " + nombreUsuario);
+                System.out.println("Contraseña: " + contraseña);
+                System.out.println("Nombre de Persona: " + nombrePersona);
+                System.out.println("Dirección de Persona: " + direccionPersona);
+                System.out.println("---------------------------");
+            }
+
+            // Cerrar el ResultSet, el PreparedStatement y la conexión cuando hayas terminado
+            resultSet.close();
+            statement.close();
+            conexion.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
